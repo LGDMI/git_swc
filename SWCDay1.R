a2abaf64e1fdf5cccdb60dafbb517e306ca8022d
Lisa Miclotte <Lisa.Miclotte@UGent.be>
2017-09-15 10:24:39 +0200

    notes in script to learn R

---
+++ SWCDay1.R
@@ -0,0 +1,65 @@
+1 + 100
+df
+1+
+kjbjdf#
+  2e5
+exp()
+log(1e4, base=10)
+log(x = 
+      
+?log
+1 ==1
+x <- 1/40
+x=1/40
+
+fm.ruben <- 42
+fm.swc <- 41
+
+#vectorization
+2^(1:5)
+x <- 1:5
+2^x
+
+
+#### environment #####
+ls(all.names = TRUE)
+
+current.environments <- ls()
+> current.environments
+
+list <- ls()
+rm(list <- ls())
+
+sessionInfo()
+
+install.packages("ggplot2")
+install.packages("plyr")
+install.packages("gapminder")
+
+
+2widths <- 654
+
+library(ggplot2)
+vignette (package = "ggplot2")
+vignette("ggplot2-specs")
+
+
+setwd (dir = "/users/lmmiclot/Documents/new data")
+
+####read out xl data #####
+
+cats <- read.csv("feline-data.csv")           ###cats = dataframe
+view(cats)
+
+read.table()   # reads tables
+
+
+cats$coat
+matrix (nrow=4,ncol=4)
+cats$weight +2
+
+typeof (cat$weight)
+typeof(cats$coat)
+
+str(cats$weight)
+
